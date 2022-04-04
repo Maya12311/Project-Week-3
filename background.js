d41648c029e3330ddf0fdd8bc27512d53aab8d2c
Mariana Schmale <marianaschmal@yahoo.dem>
2022-04-04 17:20:51 +0200

    added background images

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
 class Background{
     draw(){
+        game.backgroundImage.forEach(function (img){
+            img.x -= img.speed
+            image(img.src, img.x, 0, width, height)
+            image(img.src, img.x + width, 0, width, height)
+            if (img.x <= - width) {
+                img.x = 0
+            }
+        })
            
-     image(game.backgroundImage[0].src, 0, 0, width, height) 
+    // image(game.backgroundImage[0].src, 0, 0, width, height) 
       // console.log('this is the background')
     }
+   
 }
 
--- game.js
+++ game.js
@@ -11,7 +11,16 @@ class Game {
    preload(){
     
    this.backgroundImage = [
-       { src: loadImage('/cyberpunk-street-files/PNG/cyberpunk-street.png')}
+
+    { src: loadImage('/cyberpunk-street-files/PNG/layers/far-buildings.png'), x:0, speed: 0},
+  
+    { src: loadImage('/cyberpunk-street-files/PNG/layers/back-buildings.png'), x:0, speed: 1},
+
+    { src: loadImage('/cyberpunk-street-files/PNG/layers/foreground.png'), x:0, speed: 3},
+
+    // { src: loadImage('/cyberpunk-street-files/PNG/cyberpunk-street.png'), x:0, speed: 0},
+
+    
    ]
   // console.log('preload here in Game section')
    }
